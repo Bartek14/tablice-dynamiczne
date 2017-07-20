b72532364df4ab745efe9a994efa9eb3673730ef
Bartosz Ruszczak <bruszczak48@gmail.com>
2017-07-20 20:18:55 +0200

    Dodaj pliki projektów.

---
+++ tablice dynamiczne/tablice dynamiczne.cpp
@@ -0,0 +1,21 @@
+// tablice dynamiczne.cpp: Okreœla punkt wejœcia dla aplikacji konsoli.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int main()
+{
+	using namespace ::std;
+
+	int *ptr4 = new int[4];
+	ptr4[0] = 234;
+	ptr4[1] = 25;
+	ptr4[2] = 555;
+	ptr4[3] = 641;
+	cout << ptr4[0];
+	getchar();
+    return 0;
+}
+
