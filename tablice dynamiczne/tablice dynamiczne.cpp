e6439122fbafb916dffa36432a8e2761cc7d64e1
Bartosz Ruszczak <bruszczak48@gmail.com>
2017-07-20 20:41:38 +0200

    cout

--- tablice dynamiczne/tablice dynamiczne.cpp
+++ tablice dynamiczne/tablice dynamiczne.cpp
@@ -14,8 +14,13 @@ int main()
 	ptr4[1] = 25;
 	ptr4[2] = 555;
 	ptr4[3] = 641;
-	cout << ptr4[0];
-	getchar();
+	cout << ptr4[1]<<endl;
+	cout << ptr4<<endl;
+	ptr4++;
+	cout << ptr4[1]<<endl;
+	cout << ptr4;
+
+		getchar();
     return 0;
 }
 
